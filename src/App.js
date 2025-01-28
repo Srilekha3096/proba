80ab2370f1f8a2fdcce97f6c09e70f99d2b5dde2
Srilekha3096 <an.aesthete143@gmail.com>
2025-01-28 20:55:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './components/styles/home.css'
 //import Header from './components/pages/Header';
 import Home from './components/pages/Home';
 //import Footer from './components/pages/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
+//import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 //import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom'
+import About from './components/pages/About';
 
 function App() {
 
   return (
     <div className="App">
-<BrowserRouter>
+
+<HashRouter base="/"><Home /></HashRouter>
+<HashRouter base="/about"><About /></HashRouter>
+    
+ {/* <BrowserRouter>
         <Routes>
-          <Route path="/github.io/proba" element={<Home />} />
-          {/* <Route element={<ProtectiveRouter />}>
             <Route path="/home" exact element={<Home />} />
             <Route path="/about" exact element={<About />} />
-          </Route> */}
-        </Routes>
-      </BrowserRouter>
+          
+         </Routes> 
+       </BrowserRouter> */}
+      
     </div>
   );
 }
